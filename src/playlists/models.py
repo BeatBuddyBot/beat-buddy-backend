5805a71ee840c588f7b34171fff95f7624f78879
a.garbuzov <agarbuzov1@gmail.com>
2025-09-28 12:56:49 +0200

    Add cover_url

--- src/playlists/models.py
+++ src/playlists/models.py
@@ -1,4 +1,8 @@
+import os
+from functools import cached_property
+
 from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship
 
 from src.models import Base
@@ -13,3 +17,13 @@ class Playlist(Base):
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, server_onupdate=func.utc_timestamp())
     songs = relationship("Song", backref="playlist", cascade="all")
+
+    @cached_property
+    def bucket_domain(self):
+        return f"https://{os.getenv('AWS_BUCKET_NAME')}.s3.{os.getenv('AWS_REGION_NAME')}.amazonaws.com"
+
+    @hybrid_property
+    def cover_url(self):
+        if self.cover_key:
+            return f"{self.bucket_domain}/{self.cover_key}"
+        return None
