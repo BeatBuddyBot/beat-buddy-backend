f9bbe5da5975fc28575fa2b6e1ed04444b5faa51
a.garbuzov <agarbuzov1@gmail.com>
2025-09-28 15:23:54 +0200

    Add CORS

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.playlists.router import playlists_router
 from src.songs.router import songs_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(playlists_router)
 app.include_router(songs_router)
