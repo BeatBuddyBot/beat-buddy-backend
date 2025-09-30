97f3cf73e237c764bf31537361498bcafe0121e4
a.garbuzov <agarbuzov1@gmail.com>
2025-09-30 14:17:34 +0200

    Add url prefix

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from starlette.middleware.cors import CORSMiddleware
 
 from src.playlists.router import playlists_router
 from src.songs.router import songs_router
 
 app = FastAPI()
+api_router = APIRouter(prefix="/api/v1")
+api_router.include_router(playlists_router)
+api_router.include_router(songs_router)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,6 +16,4 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.include_router(playlists_router)
-app.include_router(songs_router)
+app.include_router(api_router)
